d17b22f821f4d1219ca9bff8abc92a67dd74d8df
Shreyash <shreyashpawar232063@gmail.com>
2022-06-17 19:32:11 +0530

    Image fetch method modified

--- src/app/model/food-model.ts
+++ src/app/model/food-model.ts
@@ -2,6 +2,7 @@ export class FoodModel {
 
   constructor(
     public id: number,
+    public image: string,
     public foodName: string,
     public foodCategory: string,
     public price: number
